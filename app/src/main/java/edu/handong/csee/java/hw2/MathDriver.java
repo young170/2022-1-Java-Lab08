8e420b039737661096a246ee412107108e64c216
김성빈 <93897642+young170@users.noreply.github.com>
2022-04-28 14:56:36 +0900

    added HW2 code

---
+++ app/src/main/java/edu/handong/csee/java/hw2/MathDriver.java
@@ -0,0 +1,102 @@
+package edu.handong.csee.java.hw2;
+
+import edu.handong.csee.java.hw2.math.MathCalculator;
+
+/**
+ * Runs the program using MathCalculator to operate.
+ * @author      Seongbin Kim
+ * @version     %I%, %G%
+ */
+public class MathDriver {
+    
+    /**
+     * Creates an instance of MathDriver to run the program.
+     * Passes values recieved from the parameter to the method run(String[] args).
+     * @param args      recieves input values that are passed to the run(String[] args) method.
+     */
+    public static void main(String[] args) {
+        MathDriver myDriver = new MathDriver();
+        myDriver.run(args);
+    }
+
+    /**
+     * Creates an instance of MathCalculator to execute the program according to the user's input(s).
+     * @param args      passed from the main(String[] args) method. args[1] contains String to set the driver's name; args[2] contains an int value for calculation; (if it is present) args[3] contains an int value for calculation.
+     */
+    public void run(String[] args) {
+
+        MathCalculator mathCalculator = new MathCalculator();
+
+        mathCalculator.setName(args[0]);
+
+        switch(args[1]) {
+            case "Max":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getMax(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Min":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getMin(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Abs":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getAbs(Integer.parseInt(args[2])));
+                break;
+            case "Sum":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getSum(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Diff":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getDiff(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Product":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getProduct(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Quotient":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getQuotient(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Remainder":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getRemainder(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Power":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getPower(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Factorial":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getFactorial(Integer.parseInt(args[2])));
+                break;
+            case "Gcd":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getGcd(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Lcm":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getLcm(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                break;
+            case "Square":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +
+                                    mathCalculator.getSquare(Integer.parseInt(args[2])));
+                break;
+            default:
+                System.out.println("Invalid command");
+        }
+    }
+}
---
+++ app/src/main/java/edu/handong/csee/java/hw2/math/MathCalculator.java
@@ -0,0 +1,182 @@
+package edu.handong.csee.java.hw2.math;
+
+/**
+ * Returns result of operation depending on the input from MathDriver.
+ * @author      Seongbin Kim
+ * @version     %I%, %G%
+ */
+public class MathCalculator {
+
+    private String name;
+
+    /**
+     * Sets the input value in the private field name.
+     * @param newName       the name to be set.
+     */
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    /**
+     * Gets the value in the private field name.
+     * @return      the current value in the private field name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Compares the values of the two inputs and returns the greater one.
+     * @param a     the first value to be compared.
+     * @param b     the second value to be compared.
+     * @return      returns the greater number between the variables.
+     */
+    public int getMax(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    /**
+     * Compares the values of the two inputs and returns the lesser one.
+     * @param a     the first value to be compared.
+     * @param b     the second value to be compared.
+     * @return      returns the lesser number between the variables.
+     */
+    public int getMin(int a, int b) {
+        return (a < b) ? a : b;
+    }
+
+    /**
+     * Gets the absolute value of the input.
+     * @param a     the value to get the absolute value.
+     * @return      returns the absolute value of the variable by multiplying -1 if it is less than 0.
+     */
+    public int getAbs(int a){
+        return (a < 0) ? a*-1 : a;
+    }
+
+    /**
+     * Gets the sum of the two inputs.
+     * @param a     the first value to be added.
+     * @param b     the second value to be added.
+     * @return      returns the sum of the two variables.
+     */
+    public int getSum(int a, int b){
+        return a+b;
+    }
+
+    /**
+     * Gets the difference of the two inputs.
+     * First, subtract the two inputs.
+     * Second, if the value difference is less, than 0 multiply -1 to make it a positive value.
+     * @param a     the first value to be compared.
+     * @param b     the second value to be compared.
+     * @return      returns the absolute value of the variable by multiplying -1 if it is less than 0.
+     */
+    public int getDiff(int a, int b) {
+        return a-b;
+    }
+
+    /**
+     * Gets the product of the two inputs.
+     * @param a     the first value to be multiplied.
+     * @param b     the second value to be multiplied.
+     * @return      returns the product by multiplying the two variables.
+     */
+    public int getProduct(int a, int b) {
+        return a*b;
+    }
+
+    /**
+     * Gets the quotient of the two inputs.
+     * @param a     the first value to be divided by.
+     * @param b     the second value to divide by.
+     * @return      returns the quotient by dividing the preceding variable by the succeeding variable.
+     */
+    public int getQuotient(int a, int b) {
+        if(b == 0) return 0;
+        return a/b;
+    }
+
+    /**
+     * Gets the remainder between the two inputs.
+     * @param a     the first value to be divided by.
+     * @param b     the second value to divide by.
+     * @return      returns the remainder when the preceding variable is divided by the succeeding variable.
+     */
+    public int getRemainder(int a, int b) {
+        if(b == 0) return 0;
+        return a%b;
+    }
+
+    /**
+     * Gets the power of an input.
+     * @param a     the base of the equation.
+     * @param b     the exponent.
+     * @return      returns the base(a) to the power of the exponent(b).
+     */
+    public int getPower(int a, int b) {
+        int result = 1;
+        if(b < 0) return -1;
+        while(b != 0) {
+            result *= a;
+            b--;
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the factorial of an input.
+     * Multiplies the input by input-n until it reaches 0.
+     * @param a     the starting value of the factorial.
+     * @return      returns the value of the factorial of the input.
+     */
+    public int getFactorial(int a) {
+        int result = 1;
+        for(int i = 1; i <= a; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the Greatest Common Divisor between two inputs.
+     * Starting from 2, until the guessed divisor is less than both numbers, end if the guessed divisor is divisible by both values.
+     * @param a     the first value to be compared.
+     * @param b     the second value to be compared.
+     * @return      returns the Greatest Common Divisor of the two values.
+     */
+    public int getGcd(int a, int b) {
+        if(b == 0) return a;
+        return getGcd(b, a%b);
+    }
+
+    /**
+     * Gets the Least Common Multiple between two inputs.
+     * Starting from the greater number, decrease the guess multiple until it is divisible by both values.
+     * @param a     the first value to be compared.
+     * @param b     the second value to be compared.
+     * @return      returns the Least Common Multiple of the two values.
+     */
+    public int getLcm(int a, int b) {
+        int result = (a > b) ? a : b;
+        while(true) {
+            if((result % a == 0) && (result % b == 0))
+                break;
+            result++;
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the square of an input.
+     * Multiplies the input by itself.
+     * @param a     the value to be squared.
+     * @return      returns the value multiplied by the value.
+     */
+    public int getSquare(int a) {
+        return a*a;
+    }
+}
